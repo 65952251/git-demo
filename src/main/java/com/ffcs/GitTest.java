919d3e702b552ac22e9d699af9f2f53d748172e2
yangj <ffyangjie@chinatelecom.cn>
2022-10-01 19:46:15 +0800

    测试提交

--- src/main/java/com/ffcs/GitTest.java
+++ src/main/java/com/ffcs/GitTest.java
@@ -7,4 +7,12 @@ public class GitTest {
         System.out.println("master 冲突提交");
         System.out.println("hot-fix 冲突提交");
     }
+
+    public void test(){
+        System.out.println("测试1");
+    }
+
+    public void test2(){
+        System.out.println("测试2");
+    }
 }
